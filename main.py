a5c9564c21d639cb7fe1647fb99344a4730d683d
aaditgargs <141548884+aaditgargs@users.noreply.github.com>
2023-08-06 20:05:22 +0530

    Update main.py
    this is the account of my family code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print('This is Manhattan ATM')
 bank_balance = 2000000
 bank_balance_bela = 1000000
+bank_balance_shaurya = 20000
 input('please enter your credit card and press enter if done')
 accout_number = input('please enter your account no. ')
 if accout_number == '20112011':
@@ -23,7 +24,16 @@ if accout_number == '20112011':
             print('Bela has received ', accout_transfer)
             print(bank_balance)
 
-elif accout_number == '166166':
+        elif accout_transfer_name == "Shaurya":
+            accout_transfer = input('how much money do you need to transfer to Shaurya ')
+            bank_balance_shaurya = int(bank_balance_shaurya) + int(accout_transfer)
+            bank_balance_bela = int(bank_balance) - int(accout_transfer)
+            print('Shaurya has received ', accout_transfer)
+            print(bank_balance)
+
+accout_number3 = input('do you want to login to an another account (y/n): ')
+
+if accout_number or accout_number3 == '166166':
     print('Welcome bela')
     print('welcome to your account')
 
@@ -43,10 +53,39 @@ elif accout_number == '166166':
                 print('Aadit has received ', accout_transfer)
                 print(bank_balance_bela)
 
+            elif accout_transfer_name == "Shaurya":
+                accout_transfer = input('how much money do you need to transfer to Shaurya ')
+                bank_balance_shaurya = int(bank_balance_shaurya) + int(accout_transfer)
+                bank_balance_bela = int(bank_balance_bela) - int(accout_transfer)
+                print('Shaurya has received ', accout_transfer)
+                print(bank_balance_bela)
 
 
+accout_number3 = input('do you want to login to an another account (y/n): ')
 
+if accout_number or accout_number3 == '20152015':
+    print('Welcome Shaurya')
+    print('welcome to your account')
 
-else:
-    print("let's continue with your day ")
+    print('your balance is $', int(bank_balance))
+    atm = input('do you want to withdraw some money (y,n) ')
+    if atm == 'y':
+        withdrawel = input('how much money do want to withdraw ')
+        bank_balance_shaurya = int(bank_balance_shaurya) - int(withdrawel)
+        print('your balance is $', bank_balance_shaurya)
+    transfer = input('do you want to transfer some money to other accounts (y,n) ')
+    if transfer == 'y':
+        accout_transfer_name = input('who do you need to transfer ')
+        if accout_transfer_name == 'Bela':
+            accout_transfer = input('how much money do you need to transfer to Bela ')
+            bank_balance = int(bank_balance_bela) + int(accout_transfer)
+            bank_balance_bela = int(bank_balance_shaurya) - int(accout_transfer)
+            print('Bela has received ', accout_transfer)
+            print(bank_balance)
 
+        elif accout_transfer_name == "Aadit":
+            accout_transfer = input('how much money do you need to transfer to Aadit ')
+            bank_balance = int(bank_balance) + int(accout_transfer)
+            bank_balance_bela = int(bank_balance_shaurya) - int(accout_transfer)
+            print('Aadit has received ', accout_transfer)
+            print(bank_balance_shaurya)
